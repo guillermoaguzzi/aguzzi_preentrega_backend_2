2af4e5db6f61112919a0e0fed409898298440f2d
Guillermo Aguzzi <114148025+guillermoaguzzi@users.noreply.github.com>
2023-07-06 00:36:36 +1000

    filters added

--- src/routes/views.products.routes.js
+++ src/routes/views.products.routes.js
@@ -13,29 +13,57 @@ class ProductsViewsRoutes {
     this.router.get(`${this.path}`, async (req, res) => {
       const limit = req.query.limit || 10;
       const category = req.query.category || null;
-
-      /* const stock = req.query.stock || null; */
-
+      const stock = req.query.stock || null;
       const sortOption = req.query.sortOption;
       const page = req.query.page || 1;
 
-
-      const categories = []
+      const categories = [];
       const noCategory = null;
-      const sortByPrice = parseInt(sortOption)
+      const sortByPrice = parseInt(sortOption);
 
       console.log(sortByPrice);
 
-      const Productscategories = await productsModel.aggregate([
+      const pipeline = [
         {
           $group: { _id: "$category" },
         },
+      ];
 
-        /* {
-          $sort: { price: sortByPrice },
-        }, */
-      ]);
-      
+      // Nuevo stage "match" si se proporciona el parámetro "stock"
+      let stockFilter = null;
+
+      if (stock === "true") {
+        stockFilter = { stock: { $gte: 1 } };
+      } else if (stock === "false") {
+        stockFilter = { stock: { $lt: 1 } };
+      }
+
+      let sort = {};
+
+      if (sortByPrice === -1) {
+        sort = { price: -1 };
+      } else if (sortByPrice === 1) {
+        sort = { price: 1 };
+      }
+
+
+/* 
+      // Nuevo stage "match" si se proporciona el parámetro "stock"
+      let priceFilter = null;
+
+      if (sortByPrice === -1) {
+        priceFilter = { $sort: { price: -1 } };
+      } else if (stock === "false") {
+        priceFilter = { $sort: { price: 1 } };
+      } */
+
+
+
+/*       if (stockFilter) {
+        pipeline.push({ $match: stockFilter });
+      } */
+
+      const Productscategories = await productsModel.aggregate(pipeline);
 
       for (let i = 0; i < Productscategories.length; i++) {
         const categoriesValues = Productscategories[i]._id;
@@ -47,7 +75,11 @@ class ProductsViewsRoutes {
       if (category !== null) {
         query.category = category;
       }
-      
+
+      if (stockFilter) {
+        query = { ...query, ...stockFilter };
+      }
+
       const categoryURL = query.category;
 
       const {
@@ -57,7 +89,7 @@ class ProductsViewsRoutes {
         nextPage,
         prevPage,
         totalPages,
-      } = await productsModel.paginate(query, { limit, page, lean: true });
+      } = await productsModel.paginate(query, { limit, page, sort, lean: true });
 
       // 2 paginas previas
       const previousPagesNumbers = [];
@@ -82,13 +114,12 @@ class ProductsViewsRoutes {
       previousPagesNumbers.sort((a, b) => a - b);
 
       // Primera y ultima pagina
-      const firstPage = 1
-      const lastPage = totalPages
-      
+      const firstPage = 1;
+      const lastPage = totalPages;
+
       const firstPageExist = page >= 4 ? firstPage : null;
       const lastPageExist =
-        followingPagesNumbers.length > 1 &&
-        totalPages - page >= 3
+        followingPagesNumbers.length > 1 && totalPages - page >= 3
           ? lastPage
           : null;
 
@@ -121,12 +152,16 @@ class ProductsViewsRoutes {
         page: page,
         hasPrevPage: hasPrevPage,
         hasNextPage: hasNextPage,
-        prevLink: hasPrevPage ? `/api/v1/view/products?limit=${limit}&page=${prevPage}&category=${category}` : null,
-        nextLink: hasNextPage ? `/api/v1/view/products?limit=${limit}&page=${nextPage}&category=${category}` : null,
+        prevLink: hasPrevPage
+          ? `/api/v1/view/products?limit=${limit}&page=${prevPage}&category=${category}`
+          : null,
+        nextLink: hasNextPage
+          ? `/api/v1/view/products?limit=${limit}&page=${nextPage}&category=${category}`
+          : null,
       };
 
-      console.log(result);
-
+      /* console.log(result); */
+      console.log(stockFilter);
     });
   }
 }
